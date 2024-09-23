bb764f55fce8a16461d17fbc91f40317a0568d31
francisca.macario <francisca.macario@defensoria.pi.def.br>
2024-09-23 11:28:10 -0300

    Java RESTful API criada para Claro Java Com Spring

---
+++ src/main/java/macario/fran/claro_java_com_spring/ClaroJavaComSpringApplication.java
@@ -0,0 +1,13 @@
+package macario.fran.claro_java_com_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaroJavaComSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaroJavaComSpringApplication.class, args);
+	}
+
+}
